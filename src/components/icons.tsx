c50b4e49d8e5e241e232c9846b27de66e2469aab
marcos rodriguez <84048657+Rodriguez-Marcos@users.noreply.github.com>
2021-11-08 15:54:14 -0300

    agregado iconos

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -6,16 +6,20 @@ import react from '../icons/react.svg'
 import nodejs from '../icons/nodejs.svg'
 import postgresql from '../icons/postgresql.svg'
 import nextjs from '../icons/nextjs.svg'
+import nestjs from '../icons/nestjs.svg'
+import express from '../icons/express.png'
 
 export default function Icons() {
     return (<div className="iconcontainer">
-        <img src={javascript.src} alt="javascripticon" className="javascripticon"/>
-        <img src={html.src} alt="htmlicon" className="htmlicon" />
-        <img src={css.src} alt="cssicon" className="cssicon" />
-        <img src={nextjs.src} alt="nextjicon" className="nextjicon" />
-        <img src={react.src} alt="reacticon" className="reacticon" />
-        <img src={typescript.src} alt="typescripticon" className="typescripticon" />
-        <img src={nodejs.src} alt="nodejicon" className="nodejicon" />
-        <img src={postgresql.src} alt="postgresqlicon" className="postgresqlicon" />
+        <dfn data-info="Javascript ES6"><img src={javascript.src} alt="javascripticon" className="javascripticon"/></dfn>
+        <dfn data-info="HTML 5"><img src={html.src} alt="htmlicon" className="htmlicon" /></dfn>
+        <dfn data-info="CSS"><img src={css.src} alt="cssicon" className="cssicon" /></dfn>
+        <dfn data-info="Nextjs"><img src={nextjs.src} alt="nextjicon" className="nextjicon" /></dfn>
+        <dfn data-info="React"><img src={react.src} alt="reacticon" className="reacticon" /></dfn>
+        <dfn data-info="Typescript"><img src={typescript.src} alt="typescripticon" className="typescripticon" /></dfn>
+        <dfn data-info="Nodejs"><img src={nodejs.src} alt="nodejicon" className="nodejicon" /></dfn>
+        <dfn data-info="Nestjs"><img src={nestjs.src} alt="nestjsicon" className="nestjs" /></dfn>
+        <dfn data-info="Express"><img src={express.src} alt="expressicon" className="express" /></dfn>
+        <dfn data-info="PostgresSQL"><img src={postgresql.src} alt="postgresqlicon" className="postgresqlicon" /></dfn>
     </div>)
 }
